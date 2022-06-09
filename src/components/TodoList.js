e7e2ffc45f4289f2f05a246b35b77866bdba0310
A1ien1385 <damian.zawadzki@tlen.pl>
2022-06-09 12:31:21 +0200

    Editing list 2

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
+import TodoForm from "./TodoForm";
 
 function TodoList() {
-  return <div>TodoList</div>;
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div>
+      <h1>What's your plan for today?</h1>
+      <TodoForm />
+    </div>
+  );
 }
 
 export default TodoList;
